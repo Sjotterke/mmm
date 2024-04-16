959f41f8d31041727c6d420d79ee3113c596a049
Sjotterke <thymenvds@hotmail.com>
2024-04-16 21:19:01 +0200

    huts

--- Em/UCHIE.py
+++ Em/UCHIE.py
@@ -4,7 +4,7 @@ from matplotlib.animation import ArtistAnimation
 from sources import GaussianSource
 from scipy.special import hankel2
 import math
-animation =True 
+animation =False
 # material properties
 ebs = 8.854 * 10**(-12) # Permittivity of free space F/m
 mu = 4 * math.pi * 10**(-7) # Permeability of free space H/m
@@ -127,11 +127,12 @@ for it in range(Nt):
     Ex[:,-1] = 0
     
     #update trackers
-    Bzt1[it] = X[Nx+1+it1[0], it1[1]]
-    Bzt2[it] = X[Nx+1+it2[0], it2[1]]
-    Bzt3[it] = X[Nx+1+it3[0], it3[1]]
+    if it>Nt//2:
+        Bzt1[it] = X[Nx+1+it1[0], it1[1]]
+        Bzt2[it] = X[Nx+1+it2[0], it2[1]]
+        Bzt3[it] = X[Nx+1+it3[0], it3[1]]
     if animation == True:
-        artists.append([plt.imshow(np.transpose(X[Nx+1:,:]), cmap='RdBu',animated=True),
+        artists.append([plt.imshow(np.transpose(X[Nx+1:,:]), cmap='viridis',vmin=-0.02*J0,vmax=0.02*J0,animated=True),
                     
                         ])
 
@@ -142,10 +143,8 @@ if animation == True:
 print("total time: {}".format(Nt*dt))
 print("X[Nx+1:] is Bz, shape: {}".format(X[Nx+1:].shape))
 
-## do fourier transform
 
 fig, axs = plt.subplots(2, 2)
-
 
 axs[0,0].set_title("Trackers")
 axs[0,0].plot(tarray, Bzt1, label="t1")
@@ -158,23 +157,104 @@ axs[0,1].plot(tarray, source(tarray))
 axs[0,1].set_xlabel("t [s]")
 axs[0,1].set_ylabel("Bz [T]")
 
+## do fourier transform of source and trackers
 source_fft = np.fft.fft(source(tarray))
 source_freq = np.fft.fftfreq(tarray.shape[-1], dt)
+Bzt1_fft = np.fft.fft(Bzt1)
+Bzt2_fft = np.fft.fft(Bzt2)
+Bzt3_fft = np.fft.fft(Bzt3)
+# index closest to omega and -omega plotting
+idxp = np.abs(source_freq).argmin()
+print(idxp)
+idxm = np.abs(source_freq).argmin()
+window = 30
+print("source_freq_shape: {}".format(source_freq.shape))
+startp = max(0, idxp-window)
+startm = max(0, idxm-window)
+endp = min(source_freq.shape[-1], idxp+window)
+endm = min(source_freq.shape[-1], idxm+window)
+print(startp, endp, startm, endm)
+print("source_freq window: {}".format(source_freq[startp: endp]))
+
+ana_fft = []
+for tracker in itlist:
+    x = abs(Nx//2-tracker[0])
+    y = abs(Ny//2 - tracker[1])
+    ana_fft.append(hankel2(0, source_freq/c * np.sqrt(x**2 + y**2)))
 
 axs[1,0].set_title("source freq transform")
-axs[1,0].plot(source_freq, source_fft, label="omega: {:2e}".format(omega))
+axs[1,0].plot(source_freq, np.abs(source_fft), label="omega: {:2e}".format(omega))
 axs[1,0].set_xlabel("omega [rad/s]")
 axs[1,0].set_ylabel("Bz [T]")
 axs[1,0].legend()
+
+axs[1,1].set_title("analytisch tracker")
+axs[1,1].plot(source_freq, np.abs(ana_fft[0]), label="t1")
+axs[1,1].plot(source_freq, np.abs(ana_fft[1]), label="t2")
+axs[1,1].plot(source_freq, np.abs(ana_fft[2]), label="t3")
+axs[1,1].legend()
 plt.show()
 
-# fig, axs = plt.subplots(2, 2)
-# for tracker in itlist:
-#     ana_fft1 = hankel2(0,)
 
+# axs[0,1].set_title("tracker fft")
+# axs[0,1].plot(source_freq, np.abs(Bzt1_fft), label="t1")
+# axs[0,1].plot(source_freq, np.abs(Bzt2_fft), label="t2")
+# axs[0,1].plot(source_freq, np.abs(Bzt3_fft), label="t3")
+# axs[0,1].legend()
+
+# Define the window around zero frequency to exclude
+window = 5* 1e10
+
+
+# Find the indices of frequencies within the window around zero
+
+exclude_indices = np.where(source_freq < window)
+filter_freq = np.delete(source_freq, exclude_indices)
+filter_source = np.delete(source_fft, exclude_indices)
+filter_ana_fft_1 = np.delete(ana_fft[0], exclude_indices)
+filter_ana_fft_2 = np.delete(ana_fft[1], exclude_indices)
+filter_ana_fft_3 = np.delete(ana_fft[2], exclude_indices)
+filter_Bzt1_fft = np.delete(Bzt1_fft, exclude_indices)
+filter_Bzt2_fft = np.delete(Bzt2_fft, exclude_indices)
+filter_Bzt3_fft = np.delete(Bzt3_fft, exclude_indices)
+
+idm = np.argmax(np.abs(filter_source))
+id_window = 5
+start = max(0, idm-id_window)
+end = min(filter_source.shape[-1], idm+id_window)
+
+fig, axs = plt.subplots(2, 2)
+
+axs[0,0].set_title("analytisch tracker filter")
+axs[0,0].plot(filter_freq[start: end], np.abs(filter_ana_fft_1)[start:end], label="t1")
+axs[0,0].plot(filter_freq[start: end], np.abs(filter_ana_fft_2)[start:end], label="t2")
+axs[0,0].plot(filter_freq[start: end], np.abs(filter_ana_fft_3)[start:end], label="t3")
+axs[0,0].legend()
+
+axs[0,1].set_title("source fft filter")
+axs[0,1].plot(filter_freq[start:end], np.abs(filter_source)[start:end])
+
+axs[1,0].set_title("tracker fft filter")
+print("filter_freq shape: {}\n filter_Bzt1_fft: {}".format(filter_freq.shape, filter_Bzt1_fft.shape))
+axs[1,0].plot(filter_freq[start:end], np.abs(filter_Bzt1_fft)[start:end], label="t1")
+axs[1,0].plot(filter_freq[start:end],  np.abs(filter_Bzt2_fft)[start:end], label="t2")
+axs[1,0].plot(filter_freq[start:end],  np.abs(filter_Bzt3_fft)[start:end], label="t3")
+axs[1,0].legend()
+
+print("compare")
+axs[1,1].set_title("(num track / source) vs ana filter")
+axs[1,1].plot(filter_freq[start:end],np.abs(filter_Bzt1_fft)[start:end],label="t1 num")
+axs[1,1].plot(filter_freq[start:end]*np.abs(filter_source)[start:end], np.abs(filter_ana_fft_1)[start:end], label="t1_ana*source")
+axs[1,1].plot(filter_freq[start:end],np.abs(filter_Bzt2_fft)[start:end], label="t1 num")
+axs[1,1].plot(filter_freq[start:end]*np.abs(filter_source)[start:end], np.abs(filter_ana_fft_2)[start:end], label="t1_ana*source")
+axs[1,1].legend()
+
+
+plt.show()
 # Bz = X[Nx+1:]
 # Bz_fft = np.fft.fft(Bz)
 # Bz_fft = np.fft.fftshift(Bz_fft)
 # freq = np.fft.fftfreq(Nt, dt)
 # plt.plot(freq, np.abs(Bz_fft))
 # plt.show()
+
--- Em/tempCodeRunnerFile.py
+++ Em/tempCodeRunnerFile.py
@@ -1,1 +1,2 @@
-fig, axs = plt.subplots(2, 2)
+axs[1,0].plot(filter_freq,  np.abs(filter_Bzt2_fft), label="t2")
+axs[1,0].plot(filter_freq,  np.abs(filter_Bzt3_fft), label="t3")
